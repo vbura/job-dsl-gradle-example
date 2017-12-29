8b725c2b3b3de6c03ab6410951bfd21d68816132
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 12:06:48 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -27,16 +27,52 @@ listView("$basePath") {
         definition {
             cps {
                 sandbox()
-                script("""
-                    node {
-                        stage 'Hello world'
-                        echo 'Hello World 1'
-                        stage "invoke another pipeline"
-                        echo 'Hello World 1'
-                        stage 'Goodbye world'
-                        echo "Goodbye world"
-                    }
-            """.stripIndent())
+                s script("""
+                     try {
+                  stage('Checkout') {
+                steps {
+                         echo 'Hello World'
+              			script {
+                         git credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
+                         def props = readProperties file: 'gradle.properties'
+                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+                          sh "git push origin HEAD:test)"
+                              }
+                    	}
+                	  }	
+				}
+                  stage ('Build') {
+                          node{
+                              sh "echo 'shell scripts to run static tests...'"
+                          }
+                      }
+                  stage ('Tests') {
+                      parallel 'static': {
+                           node{
+                                sh "echo 'shell scripts to run static tests...'"
+                           }
+                      },
+                      'unit': {
+                           node{
+                          sh "echo 'shell scripts to run unit tests...'"
+                           }
+                      },
+                      'integration': {
+                           node{
+                                  sh "echo 'shell scripts to run integration tests...'"
+                           }
+                      }
+                  }
+                  stage ('Deploy') {
+                     node{  
+                         sh "echo 'shell scripts to deploy to server...'"
+                     }
+                  }
+              } catch (err) {
+                  currentBuild.result = 'FAILED'
+                  throw err
+              }
+                """.stripIndent())
             }
         }
     }
