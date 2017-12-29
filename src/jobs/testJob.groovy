2112bd151719756cad68b597d1f671d8662aa27c
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 10:54:56 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -20,12 +20,18 @@ job("$basePath/grails example build") {
     }
     steps {
         scm {
-            git{
-                remote{
-                   url ('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
-                   credentials('t062dee70-e83b-4843-ab77-443e5fa6c7ab')
+            git {
+                remote {
+                    url('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
+                    credentials('062dee70-e83b-4843-ab77-443e5fa6c7ab')
                 }
             }
         }
     }
+    wrappers {
+        sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+            sh "git push origin HEAD:test)"
+        }
+    }
 }
+
