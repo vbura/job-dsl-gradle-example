c53d180cfa1446d2f671633b395e139a25cce9f1
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 23:09:07 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -29,7 +29,7 @@ listView("$basePath") {
                      stage("Checkout") {
                             echo 'Hello World'
                             script {
-                                git branch:"$branch" credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
+                                git branch:$branch credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
                                 def props = readProperties file: 'gradle.properties'
                                 sh "./gradlew clean"
                             }    
