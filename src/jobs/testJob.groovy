0e3e14b97b4313c17e462c4125ae03514730e249
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 09:34:23 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -1,63 +1,30 @@
+String basePath = 'example1'
+String repo = 'sheehan/grails-example'
+
 folder(basePath) {
     description 'This example shows basic folder/job creation.'
 }
 
-listView("Release") {
-    pipelineJob("testdsl-calls-other-pipeline") {
-        logRotator{
-            numToKeep 30
+job("$basePath/grails example build") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+        grails {
+            useWrapper true
+            targets(['test-app', 'war'])
         }
-        definition {
-            cps {
-                sandbox()
-                script("""
-                     try {
-                  stage('Checkout') {
-                steps {
-                         echo 'Hello World'
-              			 script {
-                         git credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
-                         def props = readProperties file: 'gradle.properties'
-                         echo "${props['version']}"
-                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
-                          sh "git push origin HEAD:props['version'])"
-                              }
-                    	}
-                	  }	
-				}
-                  stage ('Build') {
-                          node{
-                              sh "echo 'shell scripts to run static tests...'"
-                          }
-                      }
-                  stage ('Tests') {
-                      parallel 'static': {
-                           node{
-                                sh "echo 'shell scripts to run static tests...'"
-                           }
-                      },
-                      'unit': {
-                           node{
-                          sh "echo 'shell scripts to run unit tests...'"
-                           }
-                      },
-                      'integration': {
-                           node{
-                                  sh "echo 'shell scripts to run integration tests...'"
-                           }
-                      }
-                  }
-                  stage ('Deploy') {
-                     node{  
-                         sh "echo 'shell scripts to deploy to server...'"
-                     }
-                  }
-              } catch (err) {
-                  currentBuild.result = 'FAILED'
-                  throw err
-              }
-                """.stripIndent())
-            }
-        }
+    }
+}
+
+job("$basePath/grails example deploy") {
+    parameters {
+        stringParam 'host'
+    }
+    steps {
+        shell 'scp war file; restart...'
     }
 }
