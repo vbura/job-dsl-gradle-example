7fcfcfeae965f05d0ea2c1f8dd2c3a8ff86d942c
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 12:05:12 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -6,8 +6,8 @@ folder(basePath) {
 }
 
 listView("$basePath") {
-    job("$basePath/pipeline-calls-other-pipeline") {
-        description("test")
+    pipelineJob("$basePath/pipeline-calls-other-pipeline") {
+        description()
         parameters {
             stringParam('master', 'master', 'test',)
         }
@@ -15,32 +15,27 @@ listView("$basePath") {
         logRotator {
             numToKeep 30
         }
-
-        triggers {
-            scm 'H/5 * * * *'
-        }
-        steps {
-            scm {
-                git {
-                    remote {
-                        url('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
-                        credentials('062dee70-e83b-4843-ab77-443e5fa6c7ab')
-                    }
+        scm {
+            git {
+                remote {
+                    url('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
+                    credentials('062dee70-e83b-4843-ab77-443e5fa6c7ab')
                 }
             }
         }
+
         definition {
             cps {
                 sandbox()
                 script("""
-                node {
-                    stage 'Hello world'
-                    echo 'Hello World 1'
-                    stage "invoke another pipeline"
-                    echo 'Hello World 1'
-                    stage 'Goodbye world'
-                    echo "Goodbye world"
-                }
+                    node {
+                        stage 'Hello world'
+                        echo 'Hello World 1'
+                        stage "invoke another pipeline"
+                        echo 'Hello World 1'
+                        stage 'Goodbye world'
+                        echo "Goodbye world"
+                    }
             """.stripIndent())
             }
         }
