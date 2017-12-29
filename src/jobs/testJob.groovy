a00199b64c29c7c601d3e1191b76320aa8deeb2a
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 15:45:13 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -35,10 +35,14 @@ listView("$basePath") {
                             script {
                                 git credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
                                 def props = readProperties file: 'gradle.properties'
+                                sh "./gradlew release"
                             }    
                      }
                     stage ('Build') {
+                    
                          sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+                                sh "git add ."
+                                sh "git commit -am 'test'"
                                 sh "git push origin HEAD:test"
                           }
                     }
