0c27cdbd236aeba10d4e5722a69f90bb3618346b
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 15:18:16 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -28,50 +28,51 @@ listView("$basePath") {
             cps {
                 sandbox()
                 script("""
+                node {
                      try {
-                  stage('Checkout') {
-                steps {
-                         echo 'Hello World'
-              			script {
-                         git credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
-                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
-                              sh "git push origin HEAD:test)"
-                         }
-                    	}
-                	  }	
-				}
-                  stage ('Build') {
-                          node{
-                              sh "echo 'shell scripts to run static tests...'"
+                          stage('Checkout') {
+                            steps {
+                                 echo 'Hello World'
+                                script {
+                                 git credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
+                                 sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+                                      sh "git push origin HEAD:test)"
+                                 }
+                                }
+                              }	
+                        }
+                          stage ('Build') {
+                                  node{
+                                      sh "echo 'shell scripts to run static tests...'"
+                                  }
+                              }
+                          stage ('Tests') {
+                              parallel 'static': {
+                                   node{
+                                        sh "echo 'shell scripts to run static tests...'"
+                                   }
+                              },
+                              'unit': {
+                                   node{
+                                  sh "echo 'shell scripts to run unit tests...'"
+                                   }
+                              },
+                              'integration': {
+                                   node{
+                                          sh "echo 'shell scripts to run integration tests...'"
+                                   }
+                              }
                           }
+                          stage ('Deploy') {
+                             node{  
+                                 sh "echo 'shell scripts to deploy to server...'"
+                             }
+                          }
+                      } catch (err) {
+                          currentBuild.result = 'FAILED'
+                          throw err
                       }
-                  stage ('Tests') {
-                      parallel 'static': {
-                           node{
-                                sh "echo 'shell scripts to run static tests...'"
-                           }
-                      },
-                      'unit': {
-                           node{
-                          sh "echo 'shell scripts to run unit tests...'"
-                           }
-                      },
-                      'integration': {
-                           node{
-                                  sh "echo 'shell scripts to run integration tests...'"
-                           }
-                      }
-                  }
-                  stage ('Deploy') {
-                     node{  
-                         sh "echo 'shell scripts to deploy to server...'"
-                     }
-                  }
-              } catch (err) {
-                  currentBuild.result = 'FAILED'
-                  throw err
-              }
-                """.stripIndent())
+                }""".stripIndent())
             }
         }
     }
