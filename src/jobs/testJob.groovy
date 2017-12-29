1403a9c91b7913b3fd7630a410b703636a11cc23
Vlad Bura <buravlad2003@yahoo.com>
2017-12-29 16:06:31 +0200

    test job

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -8,9 +8,7 @@ folder(basePath) {
 }
 
 Build build = Executor.currentExecutor().currentExecutable as Build
- def parameter = build.getAction(ParametersAction).getParameter("Branch")
-parameter
-
+def parameter = build.getAction(ParametersAction).getParameter("Branch")
 
 listView("$basePath") {
     pipelineJob("$basePath/pipeline-calls-other-pipeline") {
