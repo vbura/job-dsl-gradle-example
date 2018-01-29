9943fb9005d79f9499539f45970ced30d351c906
Vlad Bura <vlad.bura@swisscom.com>
2018-01-29 15:10:31 +0200

    change path

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -1,5 +1,6 @@
 import groovy.io.FileType
 import hudson.model.*
+import org.apache.tools.ant.Executor
 
 String basePath = 'Release'
 
@@ -29,7 +30,7 @@ fs.each {
     println it
 }
 
-File propertiesFile = new File('bonita-adapter/gradle.properties')
+File propertiesFile = new File('/agent/workspace/dsl-test-vlad/vlad/gradle.properties')
 propertiesFile.withInputStream {
     properties.load(propertiesFile)
 }
@@ -37,46 +38,46 @@ propertiesFile.withInputStream {
 println "${properties["version"]}"
 
 
-listView("$basePath") {
-    pipelineJob("/test-release") {
-        description()
-        parameters {
-            stringParam('Branch', "$branchName", 'test',)
-        }
-        logRotator {
-            numToKeep 10
-        }
-
-        definition {
-            cps {
-                sandbox()
-                script("""
-               node {
-                     stage("Checkout") {
-                            echo 'Hello World'
-                            script {
-                                git branch:$branchName credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
-                                def props = readProperties file: 'gradle.properties'
-                                sh "./gradlew clean"
-                            }    
-                     }
-                    stage ('Build') {
-                    
-                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
-                                sh "git add ."
-                                sh "git commit -am 'test'"
-                                sh "git push origin HEAD:{$branchName}"
-                          }
-                    }
-                    stage ('Tests') {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                    stage ('Deploy') {
-                            sh "echo 'shell scripts to deploy to server...'"
-                    }
-               }
-                """.stripIndent())
-            }
-        }
-    }
-}
+//listView("$basePath") {
+//    pipelineJob("/test-release") {
+//        description()
+//        parameters {
+//            stringParam('Branch', "$branchName", 'test',)
+//        }
+//        logRotator {
+//            numToKeep 10
+//        }
+//
+//        definition {
+//            cps {
+//                sandbox()
+//                script("""
+//               node {
+//                     stage("Checkout") {
+//                            echo 'Hello World'
+//                            script {
+//                                git branch:$branchName credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
+//                                def props = readProperties file: 'gradle.properties'
+//                                sh "./gradlew clean"
+//                            }
+//                     }
+//                    stage ('Build') {
+//
+//                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+//                                sh "git add ."
+//                                sh "git commit -am 'test'"
+//                                sh "git push origin HEAD:{$branchName}"
+//                          }
+//                    }
+//                    stage ('Tests') {
+//                        sh "echo 'shell scripts to run integration tests...'"
+//                    }
+//                    stage ('Deploy') {
+//                            sh "echo 'shell scripts to deploy to server...'"
+//                    }
+//               }
+//                """.stripIndent())
+//            }
+//        }
+//    }
+//}
