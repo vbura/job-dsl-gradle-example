bf59f085efb7c014f4df4a3085ef72c6f8a5831b
Vlad Bura <vlad.bura@swisscom.com>
2018-01-29 16:09:22 +0200

    change path

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -1,4 +1,5 @@
-import groovy.io.FileType
+import hudson.FilePath;
+import jenkins.model.Jenkins;
 import hudson.model.*
 
 String basePath = 'Release'
@@ -15,58 +16,30 @@ def resolver = build.buildVariableResolver
 def branchName = resolver.resolve("Branch")
 println "${branchName}"
 
+listFiles(createFilePath(pwd()));
+
+
 
 new File("${WORKSPACE}").eachFile() { file->
     println file.getName()
 }
 
-File propertiesFile = new File('bonita-adapter/gradle.properties')
-propertiesFile.withInputStream {
-    properties.load(propertiesFile)
+
+def createFilePath(path) {
+    if (env['NODE_NAME'] == null) {
+        error "envvar NODE_NAME is not set, probably not inside an node {} or running an older version of Jenkins!";
+    } else if (env['NODE_NAME'].equals("master")) {
+        return new FilePath(path);
+    } else {
+        return new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), path);
+    }
 }
-println "${properties["version"]}"
 
 
-listView("$basePath") {
-    pipelineJob("/test-release") {
-        description()
-        parameters {
-            stringParam('Branch', "$branchName", 'test',)
-        }
-        logRotator {
-            numToKeep 10
-        }
-
-        definition {
-            cps {
-                sandbox()
-                script("""
-               node {
-                     stage("Checkout") {
-                            echo 'Hello World'
-                            script {
-                                git branch:$branchName credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
-                                def props = readProperties file: 'gradle.properties'
-                                sh "./gradlew clean"
-                            }    
-                     }
-                    stage ('Build') {
-                    
-                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
-                                sh "git add ."
-                                sh "git commit -am 'test'"
-                                sh "git push origin HEAD:{$branchName}"
-                          }
-                    }
-                    stage ('Tests') {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                    stage ('Deploy') {
-                            sh "echo 'shell scripts to deploy to server...'"
-                    }
-               }
-                """.stripIndent())
-            }
-        }
+@NonCPS
+def listFiles(rootPath) {
+    print "Files in ${rootPath}:";
+    for (subPath in rootPath.list()) {
+        echo "  ${subPath.getName()}";
     }
 }
