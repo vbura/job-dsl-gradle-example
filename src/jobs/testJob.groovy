4d841aeba01a9c2f17ac1da245c137bb5274e342
Vlad Bura <vlad.bura@swisscom.com>
2018-01-29 15:23:00 +0200

    change path

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -8,7 +8,17 @@ folder(basePath) {
     description 'This example shgows basic folder/job creation.'
 }
 
-File propertiesFile = new File('/agent/workspace/dsl-test-vlad/vlad/gradle.properties')
+
+println "WOW" + new File(".").getAbsolutePath()
+
+Build build = Executor.currentExecutor().currentExecutable as Build
+def resolver = build.buildVariableResolver
+def workspace = pwd()
+def branchName = resolver.resolve("${workspace}")
+println "${branchName}"
+
+
+File propertiesFile = new File('bonita-adapter/gradle.properties')
 propertiesFile.withInputStream {
     properties.load(propertiesFile)
 }
@@ -16,46 +26,46 @@ propertiesFile.withInputStream {
 println "${properties["version"]}"
 
 
-//listView("$basePath") {
-//    pipelineJob("/test-release") {
-//        description()
-//        parameters {
-//            stringParam('Branch', "$branchName", 'test',)
-//        }
-//        logRotator {
-//            numToKeep 10
-//        }
-//
-//        definition {
-//            cps {
-//                sandbox()
-//                script("""
-//               node {
-//                     stage("Checkout") {
-//                            echo 'Hello World'
-//                            script {
-//                                git branch:$branchName credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
-//                                def props = readProperties file: 'gradle.properties'
-//                                sh "./gradlew clean"
-//                            }
-//                     }
-//                    stage ('Build') {
-//
-//                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
-//                                sh "git add ."
-//                                sh "git commit -am 'test'"
-//                                sh "git push origin HEAD:{$branchName}"
-//                          }
-//                    }
-//                    stage ('Tests') {
-//                        sh "echo 'shell scripts to run integration tests...'"
-//                    }
-//                    stage ('Deploy') {
-//                            sh "echo 'shell scripts to deploy to server...'"
-//                    }
-//               }
-//                """.stripIndent())
-//            }
-//        }
-//    }
-//}
+listView("$basePath") {
+    pipelineJob("/test-release") {
+        description()
+        parameters {
+            stringParam('Branch', "$branchName", 'test',)
+        }
+        logRotator {
+            numToKeep 10
+        }
+
+        definition {
+            cps {
+                sandbox()
+                script("""
+               node {
+                     stage("Checkout") {
+                            echo 'Hello World'
+                            script {
+                                git branch:$branchName credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
+                                def props = readProperties file: 'gradle.properties'
+                                sh "./gradlew clean"
+                            }    
+                     }
+                    stage ('Build') {
+                    
+                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+                                sh "git add ."
+                                sh "git commit -am 'test'"
+                                sh "git push origin HEAD:{$branchName}"
+                          }
+                    }
+                    stage ('Tests') {
+                        sh "echo 'shell scripts to run integration tests...'"
+                    }
+                    stage ('Deploy') {
+                            sh "echo 'shell scripts to deploy to server...'"
+                    }
+               }
+                """.stripIndent())
+            }
+        }
+    }
+}
