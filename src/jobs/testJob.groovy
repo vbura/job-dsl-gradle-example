4016041821538d1381c615b4b85251117df53ca9
Vlad Bura <vlad.bura@swisscom.com>
2018-02-02 15:37:42 +0200

    change path

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -61,21 +61,29 @@ pipelineJob('git-duplicate') {
         numToKeep 10
     }
     definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        name('origin')
-                        url('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
-                        credentials('062dee70-e83b-4843-ab77-443e5fa6c7ab')
+        cps {
+            sandbox()
+            script("""
+               node {
+                     stage("Checkout") {
+                            echo 'Hello World'
+                            script {
+                                git credentialsId: '062dee70-e83b-4843-ab77-443e5fa6c7ab', url: 'ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git'
+                                sh " sed -i "/version=/ s/=.*/={$versionRelease}.0/" gradle.properties"
+                            }    
+                     }
+                    stage ('Build') {
+                         sshagent(['062dee70-e83b-4843-ab77-443e5fa6c7ab']) {
+                                sh "git add ."
+                                sh "git commit -am 'Create branch {$versionRelease}  by Jenkins'"
+                                sh "git push origin HEAD:releases/{$versionRelease}test"
+                          }
                     }
-                    branch('test')
-                }
-            }
-            steps {
-                shell("git push origin HEAD:"+versionRelease)
-
-            }
+                    stage ('Tests') {
+                        sh "echo 'Cleaning...'"
+                    }
+               }
+                """.stripIndent())
         }
     }
 
