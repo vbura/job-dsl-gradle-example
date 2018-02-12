6be150894b9f782398d9e89f72d77ac3ea326a91
Vlad Bura <vlad.bura@swisscom.com>
2018-02-12 14:52:27 +0200

    change path

--- src/jobs/testJob.groovy
+++ src/jobs/testJob.groovy
@@ -2,67 +2,60 @@ import groovy.io.FileType
 import hudson.model.*
 
 
-Build buildEnv = Executor.currentExecutor().currentExecutable as Build
-def resolver = buildEnv.buildVariableResolver
-def branchName = resolver.resolve("Branch")
+def file = new File('vlad/gradle.properties')
 
-println "${branchName}"
+if (file.exists()) {
+    def fileFromWorkspace = streamFileFromWorkspace('vlad/gradle.properties')
+    Properties props = new Properties()
+    props.load(fileFromWorkspace)
+    def property = props.getProperty('version')
 
-def fileFromWorkspace = streamFileFromWorkspace('vlad/gradle.properties')
-
-
-Properties props = new Properties()
-props.load(fileFromWorkspace)
-
-
-def property = props.getProperty('version')
-
-println property
-def versionRelease = property.substring(0, property.indexOf('-'))
+    println property
+    def versionRelease = property.substring(0, property.indexOf('-'))
 
 
 
-pipelineJob('taifun-core-build-' + versionRelease) {
-    description('Build aplication when a commit is made on ' + versionRelease + ' branch')
-    logRotator {
-        numToKeep 10
-    }
-    triggers {
-        bitbucketPush()
-        pollSCM {
-            scmpoll_spec('')
+    pipelineJob('taifun-core-build-' + versionRelease) {
+        description('Build aplication when a commit is made on ' + versionRelease + ' branch')
+        logRotator {
+            numToKeep 10
         }
-    }
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-
-                    remote {
-                        url('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
-                        credentials('062dee70-e83b-4843-ab77-443e5fa6c7ab')
-                    }
-                    branches('master')
-                    scriptPath('Jenkinsfile')
-                    extensions {}  // required as otherwise it may try to tag the repo, which you may not want
-                }
-
+        triggers {
+            bitbucketPush()
+            pollSCM {
+                scmpoll_spec('')
             }
         }
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+
+                        remote {
+                            url('ssh://git@git.swisscom.ch:7999/rst/bonita-adapter.git')
+                            credentials('062dee70-e83b-4843-ab77-443e5fa6c7ab')
+                        }
+                        branches('master')
+                        scriptPath('Jenkinsfile')
+                        extensions {}  // required as otherwise it may try to tag the repo, which you may not want
+                    }
+
+                }
+            }
+        }
+
     }
 
-}
-
-pipelineJob('git-duplicate') {
-    description('Build aplication when a commit is made on ' + versionRelease + ' branch')
-    logRotator {
-        numToKeep 10
-    }
-    definition {
-        cps {
-            sandbox()
-            script("""
+    pipelineJob('git-duplicate') {
+        description('Build aplication when a commit is made on ' + versionRelease + ' branch')
+        logRotator {
+            numToKeep 10
+        }
+        definition {
+            cps {
+                sandbox()
+                script("""
                node {
                      stage("Checkout") {
                             echo 'Hello World'
@@ -83,7 +76,9 @@ pipelineJob('git-duplicate') {
                     }
                }
                 """.stripIndent())
+            }
         }
     }
-
+} else {
+    println("Unable to read properties file.")
 }
